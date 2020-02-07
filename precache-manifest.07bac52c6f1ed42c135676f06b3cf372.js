d9261d9ccf032cf33298bf117f3ed27a3108537d
Ali Sanaknaki <asanaknaki@shoplogix.com>
2020-02-07 15:32:02 -0500

    Updates

---
+++ precache-manifest.07bac52c6f1ed42c135676f06b3cf372.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00404196d294beafdc7db1eaae978957",
+    "url": "/fud/index.html"
+  },
+  {
+    "revision": "dc700ebd1e927038e4d0",
+    "url": "/fud/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3bc7898e1640ffe7ef2d",
+    "url": "/fud/static/css/main.85817571.chunk.css"
+  },
+  {
+    "revision": "dc700ebd1e927038e4d0",
+    "url": "/fud/static/js/2.82540106.chunk.js"
+  },
+  {
+    "revision": "1a3ba1185bf697297fa4b180bfc93f40",
+    "url": "/fud/static/js/2.82540106.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3bc7898e1640ffe7ef2d",
+    "url": "/fud/static/js/main.e8505868.chunk.js"
+  },
+  {
+    "revision": "7ad583e9e1e65576d205",
+    "url": "/fud/static/js/runtime-main.15a43915.js"
+  },
+  {
+    "revision": "7e410ef313efe9f9a6f1c3d04d757419",
+    "url": "/fud/static/media/icon-white.7e410ef3.png"
+  }
+]);
