5dabcdfb80534f290c2524110fd6b8e3b3e03384
Ali Sanaknaki <asanaknaki@shoplogix.com>
2020-02-18 21:10:13 -0500

    Updates

--- precache-manifest.a762eb1f5d3259fd4349d9e7835cff29.js
+++ precache-manifest.a762eb1f5d3259fd4349d9e7835cff29.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "43af1fe62c9738528038fcb3766573c8",
+    "revision": "284f7842a66af011529eceff7ac436d5",
     "url": "/fud/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/fud/static/css/2.47e06e2e.chunk.css"
   },
   {
-    "revision": "98003723aacf373687f3",
+    "revision": "584051c69477c6797c9b",
     "url": "/fud/static/css/main.85817571.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/fud/static/js/2.82540106.chunk.js.LICENSE"
   },
   {
-    "revision": "98003723aacf373687f3",
-    "url": "/fud/static/js/main.80daa9ef.chunk.js"
+    "revision": "584051c69477c6797c9b",
+    "url": "/fud/static/js/main.60f00979.chunk.js"
   },
   {
     "revision": "7ad583e9e1e65576d205",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fud/precache-manifest.7d23e6b854ac8a0d26e446ca6a323e8a.js"
+  "/fud/precache-manifest.a762eb1f5d3259fd4349d9e7835cff29.js"
 );
 
 self.addEventListener('message', (event) => {
